8c8cf866b7c61afa93ee454ce45b2784c9f83881
xuehairui <931901204@qq.com>
2019-07-08 19:20:43 +0800

    springBoot

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <Description> <br>
+ *
+ * @author xue.hairui<br>
+ * @version 1.0<br>
+ * @CreateDate 2019/7/8 19:17 <br>
+ * @since 1.0<br>
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
