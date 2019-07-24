836fa1eed6b2003f03d2a6204a19902df75fe09d
xuehairui <931901204@qq.com>
2019-07-24 23:29:20 +0800

    commit

--- demo/src/main/java/com/example/controller/UserController.java
+++ demo/src/main/java/com/example/controller/UserController.java
@@ -4,19 +4,26 @@ import com.example.domain.User;
 import com.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @RequestMapping("/user")
+    public String user() {
+        return "/user/user";
+    }
 
     @RequestMapping(value = "/user/add", method = RequestMethod.GET)
     @ResponseBody
